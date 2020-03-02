0291ee2dc84621f18ebb402e1b164580fa842c75
Travis Sceroler <Jsceroler@gmail.com>
2020-03-02 11:47:29 -0600

    Took forever. Life sucks

---
+++ assets/javascript/game.js
@@ -0,0 +1,79 @@
+
+ var userWins = 0;
+ var userLosses = 0;
+ var userGuesses = [];
+ 
+ var numTries = 9;
+ guessesLeft.innerHTML = numTries;
+ wins.innerHTML = userWins;
+ losses.innerHTML = userLosses;
+ 
+ 
+ const options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", 
+ "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+ 
+ //computer choice
+ var computerChoice;
+ createComputerLetter = function(){
+  computerChoice = options[Math.floor(Math.random()* options.length)];
+ console.log(computerChoice);
+ }
+ createComputerLetter();
+ 
+ // functions
+ isGuessValid = function(guess) {
+     if(options.includes(guess)) {
+         // console.log("guess: " +  guess + " is considered valid!");
+         return true;
+     }
+     // console.log("guess: " + guess + " is not valid!");
+     return false;
+ };
+ 
+ isGuessCorrect = function (userGuess) {
+     if (userGuess === computerChoice) {
+         console.log("You're awesome!");
+         return true;
+     }
+     return false;
+ };
+ 
+ isLoss = function(){
+     if(numTries<= 0) {
+         console.log("You should feel bad");
+         return true;
+     }
+     return false;
+ }
+ 
+ //record user guess
+ document.onkeydown = function(event) {
+     const userGuessKey = event.key.toLowerCase();
+     const isValid = isGuessValid(userGuessKey);
+     if(isValid) {
+         userGuesses.push(userGuessKey);
+         guesses.textContent = userGuesses;
+         numTries--;
+         
+         // check for a win
+         if(isGuessCorrect(userGuessKey)) {
+             userWins++;
+             numTries= 9;
+             userGuesses.length = 0;
+             document.getElementById("guesses").innerHTML="";
+             createComputerLetter();
+         } else if (isLoss()) {
+             // console.log("it sure was a loss");
+             userLosses++;
+             numTries=9;
+             userGuesses.length = 0;
+             document.getElementById("guesses").innerHTML="";
+             createComputerLetter();
+         }
+ 
+     
+         guessesLeft.innerHTML = numTries;
+         wins.innerHTML = userWins;
+         losses.innerHTML = userLosses;
+     }
+ };
